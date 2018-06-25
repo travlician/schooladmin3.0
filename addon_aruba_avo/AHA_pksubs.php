66795a60800bcdc9d4641adbbccade80d7aba128
travlician <travlician@hotmail.com>
2018-06-25 13:24:50 -0400

    Additional subject package for AHA

---
+++ addon_aruba_avo/AHA_pksubs.php
@@ -0,0 +1,34 @@
+<?
+  $pksubs['01'] = array('Ne','En','Wi-A','Ak','Gs','Sp');
+  $pksubs['02'] = array('Ne','En','Wi-A','Ec','Gs','Sp');
+  $pksubs['03'] = array('Ne','En','Wi-A','Ec','Ak','Sp');
+  $pksubs['04'] = array('Ne','En','Wi-A','Ak','Gs','M&O');
+  $pksubs['05'] = array('Ne','En','Wi-A','Ec','Gs','M&O');
+  $pksubs['06'] = array('Ne','En','Wi-A','Ec','Ak','M&O');
+  $pksubs['07'] = array('Ne','En','Wi-A','Ec','Ak','Gs');
+  $pksubs['08'] = array('Ne','En','Wi-A','Ak','Gs','Bio');
+  $pksubs['09'] = array('Ne','En','Wi-A','Ec','Gs','Bio');
+  $pksubs['10'] = array('Ne','En','Wi-A','Ec','Ak','Bio');
+  $pksubs['11'] = array('Ne','En','Sp','Ak','Gs','M&O');
+  $pksubs['12'] = array('Ne','En','Sp','Ak','Gs','Ec');
+  $pksubs['13'] = array('Ne','En','Wi-A','Sk','Bio','Sp');
+  $pksubs['14'] = array('Ne','En','Wi-A','Sk','Bio','Ec');
+  $pksubs['15'] = array('Ne','En','Wi-A','Na','Sk','Sp');
+  $pksubs['16'] = array('Ne','En','Wi-A','Na','Sk','Bio');
+  $pksubs['17'] = array('Ne','En','Wi-A','Na','Sk','Ec');
+  $pksubs['18'] = array('Ne','En','Wi-B','Sk','Bio','Sp');
+  $pksubs['19'] = array('Ne','En','Wi-B','Sk','Bio','Ec');
+  $pksubs['20'] = array('Ne','En','Wi-B','Na','Sk','Sp');
+  $pksubs['21'] = array('Ne','En','Wi-B','Na','Sk','Bio');
+  $pksubs['22'] = array('Ne','En','Wi-B','Na','Sk','Ec');
+  $pksubs['23'] = array('Ne','En','Wi-A','Sk','Bio','Inf');
+	$pksubs['24'] = array('Ne','En','Wi-A','Sk','Bio','Pa');
+  // HU 90+ with CKV!
+  $pksubs['93'] = array('Ne','En','Sp','Ak','CKV','Pa');
+	$pksubs['94'] = array('Ne','En','Sp','Ak','Gs','Pa');
+	$pksubs['95'] = array('Ne','En','Sp','Ak','Gs','CKV');
+  $pksubs['96'] = array('Ne','En','Sp','Ak','CKV','Ec');
+  $pksubs['97'] = array('Ne','En','Wi-A','Ak','Gs','Ec');
+  $pksubs['98'] = array('Ne','En','Sp','Gs','CKV','Wi-A');
+  $pksubs['99'] = array('Ne','En','Sp','Ak','CKV','Wi-A');
+?>
--- addon_aruba_avo/form_Diploma_TV2_AHA.php
+++ addon_aruba_avo/form_Diploma_TV2_AHA.php
@@ -56,37 +56,7 @@
   echo '<LINK rel="stylesheet" type="text/css" href="style_Diploma_AHA.css" title="style1">';
   
   // Translation of subject package to subjects (short names)
-  $pksubs['01'] = array('Ne','En','Wi-A','Ak','Gs','Sp');
-  $pksubs['02'] = array('Ne','En','Wi-A','Ec','Gs','Sp');
-  $pksubs['03'] = array('Ne','En','Wi-A','Ec','Ak','Sp');
-  $pksubs['04'] = array('Ne','En','Wi-A','Ak','Gs','M&O');
-  $pksubs['05'] = array('Ne','En','Wi-A','Ec','Gs','M&O');
-  $pksubs['06'] = array('Ne','En','Wi-A','Ec','Ak','M&O');
-  $pksubs['07'] = array('Ne','En','Wi-A','Ec','Ak','Gs');
-  $pksubs['08'] = array('Ne','En','Wi-A','Ak','Gs','Bio');
-  $pksubs['09'] = array('Ne','En','Wi-A','Ec','Gs','Bio');
-  $pksubs['10'] = array('Ne','En','Wi-A','Ec','Ak','Bio');
-  $pksubs['11'] = array('Ne','En','Sp','Ak','Gs','M&O');
-  $pksubs['12'] = array('Ne','En','Sp','Ak','Gs','Ec');
-  $pksubs['13'] = array('Ne','En','Wi-A','Sk','Bio','Sp');
-  $pksubs['14'] = array('Ne','En','Wi-A','Sk','Bio','Ec');
-  $pksubs['15'] = array('Ne','En','Wi-A','Na','Sk','Sp');
-  $pksubs['16'] = array('Ne','En','Wi-A','Na','Sk','Bio');
-  $pksubs['17'] = array('Ne','En','Wi-A','Na','Sk','Ec');
-  $pksubs['18'] = array('Ne','En','Wi-B','Sk','Bio','Sp');
-  $pksubs['19'] = array('Ne','En','Wi-B','Sk','Bio','Ec');
-  $pksubs['20'] = array('Ne','En','Wi-B','Na','Sk','Sp');
-  $pksubs['21'] = array('Ne','En','Wi-B','Na','Sk','Bio');
-  $pksubs['22'] = array('Ne','En','Wi-B','Na','Sk','Ec');
-  $pksubs['23'] = array('Ne','En','Wi-A','Sk','Bio','Inf');
-	$pksubs['24'] = array('Ne','En','Wi-A','Sk','Bio','Pa');
-  // HU 90+ with CKV!
-	$pksubs['94'] = array('Ne','En','Sp','Ak','Gs','Pa');
-	$pksubs['95'] = array('Ne','En','Sp','Ak','Gs','CKV');
-  $pksubs['96'] = array('Ne','En','Sp','Ak','CKV','Ec');
-  $pksubs['97'] = array('Ne','En','Wi-A','Ak','Gs','Ec');
-  $pksubs['98'] = array('Ne','En','Sp','Gs','CKV','Wi-A');
-  $pksubs['99'] = array('Ne','En','Sp','Ak','CKV','Wi-A');
+require_once("AHA_pksubs.php");  
   
   $sub2full = array("Ne"=>"Nederlandse taal en literatuur", "En"=>"Engelse taal en literatuur", "Wi-A"=>"Wiskunde A",
                     "Ak"=>"Aardrijkskunde", "Gs"=>"Geschiedenis en staatsinrichting", "Sp"=>"Spaanse taal en literatuur",
